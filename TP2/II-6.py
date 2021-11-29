7c4f791b288c6a6683279577cae3ea155a1f84d4
Ubuntu <ubuntu@ubuntu2004.linuxvmimages.local>
2021-11-29 10:55:18 -0500

    II-6 fix

--- TP2/II-6.py
+++ TP2/II-6.py
@@ -5,6 +5,6 @@ if __name__ == "__main__":
     n = input("Quelle est la hauteur ?")
     for i in range(0,int(n)):
         for j in range(0,i):
-            print("  ", end="")
+            print(" ", end="")
         print("* *")
     
