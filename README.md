8876dbe99ab2b22ad0a5df901a5448cb4af6d2a8
Ewen <raoultewen@gmail.com>
2021-12-11 15:44:50 +0100

    Fixed replace all

--- README.md
+++ README.md
@@ -7,8 +7,3 @@ Consignes de dépôt :
 algo-<username_ubs_1>-<username_ubs_2>.zip
 
 Exemple : **algo-alice.e21001234-bob.e21005678.zip**
-
-TODO
-
-- Ewen
-  - Problem while replacing for example zidane by zinedine zidane in je suis zinedine zidane
--- TP3/II-5.py
+++ TP3/II-5.py
@@ -1,4 +1,29 @@
-def contains(string, substring):
+def find(tab,wanted_val):
+    """find a val in a list of integers
+
+    Args:
+        tab (list): list of integer values
+        wanted_val (int): value to search in list
+
+    Returns:
+        int: Index of wanted_val, -1 if not found
+    """
+
+    # Init the index of wanted_val to -1 in the case it's not in the list
+    index = -1
+    
+
+    # Interate through the list with index and value of each element
+    for i,val in enumerate(tab):
+
+        if val == wanted_val:
+            index = i
+            break
+    
+    return index
+
+
+def contains(string, substring,):
     """Check if a substring is included in a string, and return the start index of the substring in the string
 
     Args:
@@ -33,6 +58,50 @@ def contains(string, substring):
         return start_index
     else:
         return -1
+
+
+def contains_all(string, substring):
+    """Check for a string if it contains a substring several times. It then returns the starting index of each occurence of the substring in the string
+    It check for the first occurence with contains(), then cut string from the start to this occurence and reiterate the process until nothing is found
+
+    Args:
+        string (str): Complete string
+        substring (str): A substring to search in string
+
+    Returns:
+        list: Starting indexes of each occurence of substring in string, empty array if the substring is not found
+    """
+
+    # We get the index of the first occurence
+    found = contains(string, substring)
+
+    # Init array of found indexes
+    found_array = []
+
+    # Init a variable that gets the length of the cut part of string
+    cut_length = 0
+
+    # While there is a substring occurence found in string
+    while (found != -1):
+        #print(f'Cut length : {cut_length}, Found : {found}, Real index : {cut_length + found}')
+        
+        # We append the occurence
+        found_array.append(found+cut_length)
+
+        last_string = string
+
+        # We cut the string from the last char of the current substring occurence
+        string = string[found+len(substring):]
+
+        # And we calculate the new cut length
+        cut_length += len(last_string) - len(string)
+
+        # Finally, we look for the next occurence
+        found = contains(string, substring)
+        
+
+    return found_array
+
 
 def replace(text,old,new):
     """Replace first occurence of old string by new string in text
@@ -87,20 +156,34 @@ def replace_all(text,old,new):
         str: new version of the text
     """
     
-    # We find first occurence of old string in text
-    
-    found = contains(text, old) == -1
+    # Init new version of text
+    new_text = ""
 
-    # While there is an occurence,
-    while found:
+    # Array of starting index of every occurence of old string in text
+    found_start_indexes = contains_all(text,old)
 
-        # We replace this occurence by new text
-        text = replace(text, old, new)
+    # Init starting and ending index of current occurence
+    start_index = -1
+    end_index = -1
 
-        # And we find next occurence
-        found = contains(text, old) == -1
+    for i,c in enumerate(text):
+        
+        # If current index is part of starting indexes of old string occurences,
+        if find(found_start_indexes,i) != -1:
 
-    return text
+            # We change start and ending index values
+            start_index = i
+            end_index = i + len(old)
+
+            # And we add new string to new version of text
+            new_text += new
+
+        # If current char is not whithin an occurence's indexes, we add it to the new string
+        if not (i >= start_index and i < end_index):
+            new_text += c
+
+    return new_text
+
 
 if __name__ == '__main__':
 
